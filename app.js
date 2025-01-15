5f41eb51a85c4dcdc5e8f3311534d349a55ab2bc
Helder Balbino <helderbalbino@googlemail.com>
2025-01-15 12:38:50 +0000

    add javaScript basic logic

---
+++ app.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const newToDo = document.getElementById("new-todo");
+    const addButton = document.getElementById("")("add-button");
+    const toDoList = document.getElementById("todo-list");
+});
